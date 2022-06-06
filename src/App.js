87ac0cfc026defd866a08cc299d57ebb28697e73
luuthihongtham <luuthihongtham@ginno.com>
2022-06-06 16:45:07 +0700

    Bai 63: Cau hinh Router/Layout cho du an

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import { Route, Routes, Link } from 'react-router-dom';
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from '~/components/Layout';
 
 function App() {
     return (
-        <div className="App">
-            <h1>React App</h1>
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        // const Layout = route.layout === null ? Fragment : DefaultLayout;
+                        let Layout = DefaultLayout;
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+
+                        const Page = route.component;
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            ></Route>
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
